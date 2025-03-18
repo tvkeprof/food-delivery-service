1a1fb56b8e8aa2c9f641c1ee43f06353e3f21ce4
tvkeprof <wefousoul@gmail.com>
2025-03-18 19:26:21 +0800

    d

--- src/controllers/users/logInUser.controller.js
+++ src/controllers/users/logInUser.controller.js
@@ -1,30 +1,54 @@
 import { Users } from "../../models/users.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const logInUser = async (req, res) => {
   const { email, password } = req.body;
 
-  const findEmail = await Users.findOne({ email });
   try {
+    const findEmail = await Users.findOne({ email });
+
     if (!findEmail) {
       return res.status(404).json({
-        message: " user not found",
+        message: "User not found",
       });
     }
+
     const match = await bcrypt.compare(password, findEmail.password);
 
-    if (match) {
-      return res.status(200).json({
-        succes: true,
-        data: findEmail,
-      });
-    } else {
+    if (!match) {
       return res.status(400).json({
         success: false,
-        message: "wrong password",
+        message: "Wrong password",
       });
     }
-  } catch (error) {}
 
-  res.json("invalid password");
+    const token = jwt.sign(
+      {
+        id: findEmail._id,
+        email: findEmail.email,
+        role: findEmail.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "2h" }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: findEmail._id,
+        email: findEmail.email,
+        role: findEmail.role,
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
 };
