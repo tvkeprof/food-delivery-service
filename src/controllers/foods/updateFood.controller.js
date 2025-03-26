f63e6c4d8fa0ff37bcde016fea0583949c248513
tvkeprof <wefousoul@gmail.com>
2025-03-26 18:32:41 +0800

    d

--- src/controllers/foods/updateFood.controller.js
+++ src/controllers/foods/updateFood.controller.js
@@ -19,3 +19,42 @@ export const updateFoods = async (req, res) => {
     });
   }
 };
+// import multer from "multer";
+// import { Foods } from "../../models/food.models.js";
+
+// const upload = multer({ dest: "uploads/" });
+
+// export const updateFoods = async (req, res) => {
+//   const { id } = req.params;
+//   const { foodName, price, ingredients } = req.body;
+//   let imagePath = req.body.image;
+//   if (req.file) {
+//     imagePath = req.file.path;
+//   }
+
+//   try {
+//     const updatedFood = await Foods.findByIdAndUpdate(
+//       id,
+//       {
+//         foodName,
+//         price,
+//         ingredients,
+//         image: imagePath,
+//       },
+//       { new: true }
+//     );
+
+//     if (!updatedFood) {
+//       return res.status(404).json({ message: "Food item not found" });
+//     }
+
+//     res.status(200).json({ updatedFood });
+//     console.log("Updated food:", updatedFood);
+//   } catch (err) {
+//     console.log("Error updating food:", err);
+//     res.status(400).json({
+//       message: "Error while updating food",
+//       error: err.message,
+//     });
+//   }
+// };
--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -4,10 +4,15 @@ import { getFood } from "../controllers/foods/getFoodById.controller.js";
 import { createFood } from "../controllers/foods/createfood.controller.js";
 import { updateFoods } from "../controllers/foods/updateFood.controller.js";
 import { deleteFoods } from "../controllers/foods/deleteFood.controller.js";
+// import multer from "multer";
 export const foodRouter = Router();
-
+// const upload = multer({ dest: "uploads/" });
 foodRouter.get("", getFoods);
 foodRouter.get("/:id", getFood);
 foodRouter.post("", createFood);
 foodRouter.delete("/:id", deleteFoods);
-foodRouter.put("/:id", updateFoods);
+foodRouter.put(
+  "/:id",
+  //  upload.single("image"),
+  updateFoods
+);
