e0bfcf22057e9349f675dbf29e004ec39c9fbc62
tvkeprof <wefousoul@gmail.com>
2025-04-03 18:05:34 +0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { categoryRouter } from "./routes/category.routes.js";
-import { userRouter } from "./routes/user.routes.js";
-import { orderRouter } from "./routes/order.routes.js";
-import { foodRouter } from "./routes/food.routes.js";
+import { categoryRouter } from "./src/routes/category.routes.js";
+import { userRouter } from "./src/routes/user.routes.js";
+import { orderRouter } from "./src/routes/order.routes.js";
+import { foodRouter } from "./src/routes/food.routes.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import { logInRouter } from "./routes/logIn.routes.js";
+import { logInRouter } from "./src/routes/logIn.routes.js";
 
 dotenv.config();
 
--- src/models/foodOrder.model.js
+++ src/models/foodOrder.model.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const foodOrderSchema = new mongoose.Schema({
-  user: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
-  totalPrice: { type: Number, required: true },
-  image: { type: String, required: true },
-  foodOrderItems: [
-    {
-      food: { type: mongoose.Types.ObjectId, required: true, ref: "foods" },
-      quantity: { type: Number, required: true },
+const foodOrderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
+    totalPrice: { type: Number, required: true },
+    image: { type: String, required: true },
+    foodOrderItems: [
+      {
+        food: { type: mongoose.Types.ObjectId, required: true, ref: "foods" },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["PENDING", "CANCELLED", "DELIVERED"],
+      required: true,
     },
-  ],
-  status: {
-    type: String,
-    enum: ["PENDING", "CANCELLED", "DELIVERED"],
-    required: true,
   },
-});
+  { timestamps: true }
+);
 export const Orders = mongoose.model("foodOrder", foodOrderSchema);
